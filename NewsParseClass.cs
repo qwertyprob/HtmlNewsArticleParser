08942040e3cbfabe06e9c365c4afaa6799a9cd65
Arsenii Arlech1no <arsenii.subenok@iis.utm.md>
2024-10-10 16:22:43 +0300

    Refactor

--- NewsParseClass.cs
+++ NewsParseClass.cs
@@ -8,12 +8,27 @@ namespace ParserHtml
 {
     public class NewsParseClass
     {
-        public string Theme { get; set; }
-        public string Title { get; set; }
-        public string ImageUrl { get; set; }
-        public string AltText { get; set; }
-        public string PublishTime { get; set; }
-        public DateTime ParseDate { get; set; }
-        public string Content { get; set; }
+        public string Theme { get; set; } = "No theme";
+        public string Title { get; set; } = "No title";
+        public string ImageUrl { get; set; } = "No Image Url";
+        public string AltText { get; set; } = "No Text from Img";
+        public string PublishTime { get; set; } = "No Time";
+        public DateTime ParseDate { get; set; } = DateTime.Now;
+        public string Content { get; set; } = "No text";
+
+        public string url = "https://zugo.md/";
+        public string[] SetArrayOfPages(int counter)
+        {
+            string[] pages = new string[counter];
+
+            for(int i = counter-1; i>=0; i--)
+            {
+                pages[i] = url + $"page/{i+1}";
+
+            }
+
+            return pages;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,50 +1,16 @@
-﻿using System;
-using HtmlAgilityPack;
-using Newtonsoft.Json; 
-using System.IO;
+﻿//MAIN FILE!!!!!
+
+
 using ParserHtml;
-using System.Text;
 
-var jsonArray = new List<NewsParseClass>();
-
-var jsonElemList = BigMehod.BigMethodRealization("https://zugo.md/");
-
-var jsonElemListSecondPage = BigMehod.BigMethodRealization("https://zugo.md/page/2");
-
-var jsonElemListThirdPage = BigMehod.BigMethodRealization("https://zugo.md/page/3");
-
-    foreach (var item in jsonElemList)
-    {
-       jsonArray.Add(item);
-    }
-    foreach (var item in jsonElemListSecondPage)
-    {
-        jsonArray.Add(item);
-    }
-    foreach (var item in jsonElemListThirdPage)
-    {
-        jsonArray.Add(item);
-    }
+var pe = new NewsParseClass();
 
 
-//JsonParse 
+var arr=pe.SetArrayOfPages(3);
 
-string json = JsonConvert.SerializeObject(jsonElemList, Formatting.Indented);
-Console.WriteLine(json);
 
-using (var fs = new FileStream("C:\\Users\\eqspe\\source\\repos\\parser\\news.json", FileMode.Append, FileAccess.Write))
+foreach(var item in arr)
 {
-    // Convert the JSON string to a byte array
-    byte[] jsonBytes = System.Text.Encoding.UTF8.GetBytes(json);
-
-    // Optionally add a newline before appending new data
-    byte[] newlineBytes = System.Text.Encoding.UTF8.GetBytes(Environment.NewLine);
-    fs.Write(newlineBytes, 0, newlineBytes.Length); // Write newline to separate entries
-
-    // Write the byte array to the file
-    fs.Write(jsonBytes, 0, jsonBytes.Length);
+    Console.WriteLine(item);
 }
-
-
-
 
